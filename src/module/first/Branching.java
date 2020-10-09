a6b3cc70ae986d16fe03629a0b2a126937f1ee19
Alexey Galkin <galkinlx@tut.by>
2020-10-10 00:19:30 +0300

    module1 task2.3 in progress 2

--- src/module/first/Branching.java
+++ src/module/first/Branching.java
@@ -1,13 +1,14 @@
 package module.first;
 
 import java.sql.PreparedStatement;
+import java.util.Scanner;
 
 public class Branching {
     public static void main(String[] args) {
 //    .
         task1_1();
         task2_2();
-
+        task2_3();
     }
 
     /*
@@ -38,28 +39,53 @@ public class Branching {
         int b = 20;
         int c = -15;
         int d = 44;
-        int minAB = compareForMin(a,b);
-        int minCD = compareForMin(c,d);
-        int result = Math.max(minAB,minCD);
-        System.out.println("2. Result module1 task2_2  MAX is: "+result);
-
-
+        int minAB = compareForMin(a, b);
+        int minCD = compareForMin(c, d);
+        int result = Math.max(minAB, minCD); //тренировался через класс Match
+        System.out.println("2. Result module1 task2_2  MAX is: " + result);
     }
 
+    //тренировался, создал метод
     public static int compareForMin(int value1, int value2) {
-        if (value1<value2){
-            return  value1;
-        }else
+        if (value1 < value2) {
+            return value1;
+        } else
             return value2;
-
-     //   return value1;
     }
 
-//            3. Даны три точки А(х1,у1), В(х2,у2) и С(х3,у3). Определить, будут ли они расположены на одной прямой.
+
+    /*3. Даны три точки А(х1,у1), В(х2,у2) и С(х3,у3). Определить, будут ли они расположены на одной прямой.
+
+Есть математическое выражение (x1 - x2) * (y1 - y3) == (x1 - x3) * (y1 - y2) если true---значит это линейная функция
+и три точки лежат на прямой
+
+Как реализовать? Можно класс с конструктором  и точками x,y...или массивы иксов, игриков.. но тут же  внутри метода...
+Прописал значения для true
+
+         **/
+    public static void task2_3() {
+        int x1 = -4;
+        int x2 = 1;
+        int x3 = 6;
+        int y1 = -2;
+        int y2 = 1;
+        int y3 = 4;
+
+        boolean result = (x1 - x2) * (y1 - y3) == (x1 - x3) * (y1 - y2);
+        System.out.println(result);
+
+
+    }
+
+
+
+
+
+}
+
 
 //4. Заданы размеры А, В прямоугольного отверстия и размеры х, у, z кирпича. Определить, пройдет ли кирпич через отверстие.
 
 //5. Вычислить значение функции:
 
 
-}
