68af1d47ca423863a7ea7100cd66f2ee962c0d55
Alexey Galkin <galkinlx@tut.by>
2020-10-15 14:57:30 +0300

    started second module
    Signed-off-by: Alexey Galkin <galkinlx@tut.by>

--- src/module/first/LinearPrograms.java
+++ src/module/first/LinearPrograms.java
@@ -17,6 +17,7 @@ public class LinearPrograms {
         c = 3; //переинициализация переменной
         a = b - 5;
 
+
         double result2 = (b + Math.sqrt(Math.pow(b, 2) + 4 * a * c)) / (2 * a) - Math.pow(a, 3) * c + Math.pow(b, -2);
         System.out.println("2. Result LinearPrograms task2: " + result2);
 
