5309b14f555ccb0df4bf21a103423775b77ceb67
Alexey Galkin <galkinlx@tut.by>
2020-10-27 20:30:25 +0300

    module 2 task2: add magic squere for odd number!
    Signed-off-by: Alexey Galkin <galkinlx@tut.by>

--- src/module/second/onedimencionalaarays/ArraysOfArrays.java
+++ src/module/second/onedimencionalaarays/ArraysOfArrays.java
@@ -79,6 +79,8 @@ public class ArraysOfArrays {
         task2_13(arr);
         task2_14();
         task2_15(arr);
+        task2_16();
+
 
     }
 
@@ -383,5 +385,37 @@ public class ArraysOfArrays {
         for (int i = 0; i < array.length; i++) {
             System.out.println(Arrays.toString(array[i]));
         }
+    }
+    static void task2_16(){
+
+        int n = 5;
+        if (n % 2 == 0) throw new RuntimeException("n must be odd");
+
+        int[][] magic = new int[n][n];
+
+        int row = n-1;
+        int col = n/2;
+        magic[row][col] = 1;
+
+        for (int i = 2; i <= n*n; i++) {
+            if (magic[(row + 1) % n][(col + 1) % n] == 0) {
+                row = (row + 1) % n;
+                col = (col + 1) % n;
+            }
+            else {
+                row = (row - 1 + n) % n;
+            }
+            magic[row][col] = i;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (magic[i][j] < 10)  System.out.print(" ");  // for alignment
+                if (magic[i][j] < 100) System.out.print(" ");  // for alignment
+                System.out.print(magic[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
     }
 }
