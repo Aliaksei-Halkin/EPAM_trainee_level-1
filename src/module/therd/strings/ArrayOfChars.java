8537f5850bd4654685f0c6e2aa7743bd7fb3a965
Alexey Galkin <galkinlx@tut.by>
2020-10-30 17:02:22 +0300

    module 3_1 task 4 done!
    Signed-off-by: Alexey Galkin <galkinlx@tut.by>

--- src/module/therd/strings/ArrayOfChars.java
+++ src/module/therd/strings/ArrayOfChars.java
@@ -19,19 +19,21 @@ import java.util.Arrays;
 public class ArrayOfChars {
 
     public static void main(String[] args) {
-        //1.11
+        //1.1
         String[] camelCase = new String[]{"HelloWorld", "SeeALotOfCamel", "CamelAnywhereAndAnything", "CoodbuyCamel"};
 
         System.out.println(Arrays.toString(camelCase));
         System.out.println(Arrays.toString(runTask1_1(camelCase)));
-        //1.12
+        //1.2
         String str = "word  camelword  camel word  camelwordword  camel  ";
         String str2 = str.replaceAll("word", "letter");
         System.out.println(str2);
-        //1.13
+        //1.3
         String stringForTask3 = "1234f5f6f78f9fds0vb1e2asf3sf4sf5sf6sc7sf8xdsg9c0";
-        System.out.println("1.13 The Result, quantity of numbers is "+runTask1_3(stringForTask3));
+        System.out.println("1.3 The Result, quantity of numbers is " + runTask1_3(stringForTask3));
+        //1.4
 
+        System.out.println("1.4 The Result, quantity of numbers is " + runTask1_4(stringForTask3));
     }
 
     static String[] runTask1_1(String[] array) {
@@ -59,12 +61,26 @@ public class ArrayOfChars {
     static int runTask1_3(String str) {
         int counter = 0;
         for (int i = 0; i < str.length(); i++) {
-            char num= str.charAt(i);
-            if(Character.isDigit(num)==true){
+            char num = str.charAt(i);
+            if (Character.isDigit(num) == true) {
                 counter++;
             }
         }
         return counter;
+    }
+
+    static int runTask1_4(String str) {
+        int counter = 0;
+        for (int i = 0; i < str.length()-1; i++) {
+            char num = str.charAt(i);
+            char num2 = str.charAt(i + 1);
+
+            if (Character.isDigit(num) == true && Character.isDigit(num2) == false) {
+                counter++;
+            }
+        }
+        return counter;
+
     }
 
 }
