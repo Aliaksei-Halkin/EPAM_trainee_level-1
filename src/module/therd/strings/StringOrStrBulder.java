a144f13992fb5d3e999912e918367ade5504ae71
Alexey Galkin <galkinlx@tut.by>
2020-11-01 17:10:51 +0300

    восстановил модуль 2 из блокнота
    Signed-off-by: Alexey Galkin <galkinlx@tut.by>

--- src/module/therd/strings/StringOrStrBulder.java
+++ src/module/therd/strings/StringOrStrBulder.java
@@ -34,6 +34,24 @@ public class StringOrStrBulder {
         String str2 = "And from Nevada, the money trail leads to Chongqing City," +
                 " the pharma capital of the People's Republic of China";
         System.out.println("Result sentences(after letter \"a\" include letter \"b\": " + runTask2_2(str2));
+//2.3
+        String str3 = "Madam";
+        System.out.println("Result,Is it polindrom?  " + runTask2_3(str3));
+        //2.4
+        String str4 = "информатика";
+
+        String result = "" + str4.charAt(str4.indexOf("т")) + str4.substring(str4.indexOf("ор"), str4.indexOf("ор") + 2) +
+                str4.charAt(str4.indexOf("т"));
+        System.out.println("Result task2_4: " + result);
+        //2.5
+        String str5 = "Подсчитать, сколько раз среди символов заданной строки встречается буква “а”";
+        System.out.println("Result: " + runTask2_5(str5));
+        //2.6
+        String str6 = "повторить дважды";
+        System.out.println("Result 2.6 : " + runTask2_6(str6));
+        //2.7
+        String str7 = "  jja   av v aa a vir  ttt  tuu  ual macc  cchine  e   ";
+        System.out.println("Result 2.7: " + runTask2_7(str7));
 
     }
 
@@ -63,5 +81,55 @@ public class StringOrStrBulder {
             }
         }
                return new String(stringBuilder);
+    }
+    private static boolean runTask2_3(String sentences) {
+        boolean firstBoolean = false;
+        boolean secondBoolean = true;
+        sentences = sentences.toLowerCase();
+        for (int i = 0; i < sentences.length(); i++) {
+            int j = sentences.length() - 1 - i;
+            if (sentences.charAt(i) == sentences.charAt(j)) {
+                firstBoolean = true;
+            } else {
+                secondBoolean = false;
+            }
+        }
+        return firstBoolean && secondBoolean;
+    }
+
+    private static int runTask2_5(String str) {
+        int counter = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'а') {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    private static String runTask2_6(String str) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = 0; j < 2; j++) {
+                stringBuilder.append(str.charAt(i));
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private static String runTask2_7(String str) {
+        StringBuilder stringBuilder = new StringBuilder();
+        str = str.trim();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ' '){
+                stringBuilder.append(str.charAt(i));
+            }
+        }
+        for (int i = stringBuilder.length()-1; i > 0; i--) {
+            if (stringBuilder.charAt(i)==stringBuilder.charAt(i-1)){
+                stringBuilder.deleteCharAt(i);
+            }
+        }
+        return stringBuilder.toString();
     }
 }
