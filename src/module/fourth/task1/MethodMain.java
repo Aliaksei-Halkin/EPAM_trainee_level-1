3b9a1095b5680adbb091cbfcc62d01494bdd53a6
Alexey Galkin <galkinlx@tut.by>
2020-11-06 23:11:32 +0300

    возникло хлопотное дельце
    Signed-off-by: Alexey Galkin <galkinlx@tut.by>

--- src/module/fourth/task1/MethodMain.java
+++ src/module/fourth/task1/MethodMain.java
@@ -2,4 +2,11 @@ package module.fourth.task1;
 
 public class MethodMain {
 
+    public static void main(String[] args) {
+        Test1 firstObject = new Test1();
+        Test1 secondObject = new Test1(10, 20);
+        Test1 therdObject = new Test1();
+        therdObject.setNumberOne(30);
+        therdObject.setNumberTwo(40);
+    }
 }
