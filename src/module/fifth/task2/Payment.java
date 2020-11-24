2db4cb3876f9bea8938ce3dd53d9a146902fb4ba
Alexey Galkin <galkinlx@tut.by>
2020-11-24 18:32:04 +0300

    fix
    Signed-off-by: Alexey Galkin <galkinlx@tut.by>

--- src/module/fifth/task2/Payment.java
+++ src/module/fifth/task2/Payment.java
@@ -20,6 +20,30 @@ public class Payment {
         this.order = order;
     }
 
+    public int getCash() {
+        return cash;
+    }
+
+    public void setCash(int cash) {
+        this.cash = cash;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
     public class Order {
         double weight;
         double value;
--- src/module/fifth/task2/RunMain.java
+++ src/module/fifth/task2/RunMain.java
@@ -2,9 +2,11 @@ package module.fifth.task2;
 
 public class RunMain {
     public static void main(String[] args) {
-       // Payment.Order carrots=new Payment.Order();
-        //Payment.Order potatos=new Payment.Order(1.0,0.7);
-        //Payment.Order orange=new Payment.Order(1.0,0.7);
+        Payment payment=new Payment();
+        payment.setOrder(  new Payment.Order(1.0,0.7));
+
+        Payment.Order potatos=new Payment.Order(1.0,0.7);
+        Payment.Order orange=new Payment.Order(1.0,0.7);
 
 
     }
